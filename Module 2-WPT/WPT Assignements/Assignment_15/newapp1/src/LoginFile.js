6baca4d44aa607e4dca032567cec314d04392800
Asit More <136837500+MADARA723@users.noreply.github.com>
2026-03-20 22:33:48 +0530

    Update LoginFile.js

--- Module 2-WPT/WPT Assignements/Assignment_15/newapp1/src/LoginFile.js
+++ Module 2-WPT/WPT Assignements/Assignment_15/newapp1/src/LoginFile.js
@@ -1,90 +1,55 @@
-// 2) Create LoginForm using class component 
-//     accept username and password 
-//     and display
-// 	 hello <username>  on same page 
-
-// 	if username = "iet" and password = "iet"
-
-
 import { Component } from "react";
 
-//JSX 
 class Login extends Component {
 
     constructor() {
         super();
-        //this.msg = "Default Msg"// instance 
 
-        this.state = { msg: "default" }//state 
+        this.state = {
+            username: "",
+            password: "",
+            msg: ""
+        };
 
         this.textHandler = this.textHandler.bind(this);
-          this.passwordHandler = this.passwordHandler.bind(this);
-
-        //bind the event with current object variable 
-
+        this.passwordHandler = this.passwordHandler.bind(this);
+        this.validator = this.validator.bind(this);
     }
 
-    btnHandler() {
-
-        console.log("btnHandler")
-
-    }
-    // reading textfield data 
-    //event 
     textHandler(e) {
-        // console.log("textHandler")
-        console.log(e.target);// element 
-        console.log(e.target.name);
-        
-        console.log(e.target.value);// text onBlur 
-        //this.msg = "New Msg " + e.target.value;
-        this.setState({ msg: e.target.value })
-        // instance 
-        // bind problem   error undefined 
-        return e.target.value;
-
+        this.setState({ username: e.target.value });
     }
 
-    passwordHandler(i){
-        console.log(i.target);
-        console.log(i.target.password);
-        console.log(i.target.value);
-       return i.target.value; 
+    passwordHandler(e) {
+        this.setState({ password: e.target.value });
     }
-    validator()
-    {
-        if(this.textHandler=="iet" && this.passwordHandler=="iet")
-        {
-            alert("Login Succesfull");
-        }
-        else
-        {
-            alert("Invalid credentials");
+
+    validator() {
+        if (this.state.username === "iet" && this.state.password === "iet") {
+            this.setState({ msg: "Login Successful" });
+            alert("Login Successful");
+        } else {
+            this.setState({ msg: "Invalid Credentials" });
+            alert("Invalid Credentials");
         }
     }
 
     render() {
-
         return <>
-            <h1>Login Form </h1>
-            Enter UserName <input type="text"
-                name="username" onBlur={this.textHandler} /><br />
-        
-            <p>Hello {this.state.msg}</p>
+            <h1>Login Form</h1>
 
-            
-            <h1>password</h1>
-            Enter password
+            Enter Username:
+            <input type="text" onChange={this.textHandler} /><br />
 
-            <input type="text" name="Password" onBlur={this.passwordHandler} /><br />
+            Enter Password:
+            <input type="password" onChange={this.passwordHandler} /><br />
 
-            <input type="button" value={"Login"}
-                onClick={this.validator} />
-            
+            <input type="button" value="Login" onClick={this.validator} />
+
+            <p>Hello {this.state.username}</p>
+            <p>{this.state.msg}</p>
         </>
-
-
     }
 }
 
-export default Login
+export default Login;
