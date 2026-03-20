de5314b4a0180a9e39636ffa75187a0f3468c978
Asit More <136837500+MADARA723@users.noreply.github.com>
2026-03-20 14:52:34 +0530

    Refactor LoginForm to use class component

--- Module 2-WPT/WPT Assignements/Assignment_15/newapp1/src/LoginFile.js
+++ Module 2-WPT/WPT Assignements/Assignment_15/newapp1/src/LoginFile.js
@@ -6,43 +6,85 @@
 // 	if username = "iet" and password = "iet"
 
 
-import {Component} from ("react");
+import { Component } from "react";
 
-function LoginForm()
-{
-    constructor(){
+//JSX 
+class Login extends Component {
+
+    constructor() {
         super();
+        //this.msg = "Default Msg"// instance 
 
-        this.msg
-    }
-    btnHandler()
-    {
-    
+        this.state = { msg: "default" }//state 
 
+        this.textHandler = this.textHandler.bind(this);
+          this.passwordHandler = this.passwordHandler.bind(this);
 
-
-    }
-
-
-
-    textHandler(e)
-    {
-        console.log(e.target);
-
-
+        //bind the event with current object variable 
 
     }
-    render()
+
+    btnHandler() {
+
+        console.log("btnHandler")
+
+    }
+    // reading textfield data 
+    //event 
+    textHandler(e) {
+        // console.log("textHandler")
+        console.log(e.target);// element 
+        console.log(e.target.name);
+        
+        console.log(e.target.value);// text onBlur 
+        //this.msg = "New Msg " + e.target.value;
+        this.setState({ msg: e.target.value })
+        // instance 
+        // bind problem   error undefined 
+        return e.target.value;
+
+    }
+
+    passwordHandler(i){
+        console.log(i.target);
+        console.log(i.target.password);
+        console.log(i.target.value);
+       return i.target.value; 
+    }
+    validator()
     {
-        return(
-            <div>
-                <input type="text" name="username"/>
-                <input type="text" name="Password"/>
+        if(this.textHandler=="iet" && this.passwordHandler=="iet")
+        {
+            alert("Login Succesfull");
+        }
+        else
+        {
+            alert("Invalid credentials");
+        }
+    }
+
+    render() {
+
+        return <>
+            <h1>Login Form </h1>
+            Enter UserName <input type="text"
+                name="username" onBlur={this.textHandler} /><br />
+        
+            <p>Hello {this.state.msg}</p>
+
+            
+            <h1>password</h1>
+            Enter password
+
+            <input type="text" name="Password" onBlur={this.passwordHandler} /><br />
+
+            <input type="button" value={"Login"}
+                onClick={this.validator} />
+            
+        </>
 
 
-
-            </div>
-
-        )
     }
 }
+
+export default Login
