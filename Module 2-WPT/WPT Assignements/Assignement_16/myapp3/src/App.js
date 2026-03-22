093d805e5518786d12b8b0bdaa9526cf2e13cfd8
Asit More <136837500+MADARA723@users.noreply.github.com>
2026-03-22 17:20:26 +0530

    Integrate ShopStock component into App
    Added ShopStock component and updated imports.

--- Module 2-WPT/WPT Assignements/Assignement_16/myapp3/src/App.js
+++ Module 2-WPT/WPT Assignements/Assignement_16/myapp3/src/App.js
@@ -1,18 +1,28 @@
 
 import './App.css';
-import CounterFunc from './CounterFunc'
-import DropDown from './DataDropD'
-import Parent from './1_Parent'
-import FuncLife from './FuncLifeUseE'
+import CounterFunc from './CounterFunc';
+import DropDown from './DataDropD';
+import Parent from './Parent';
+import FuncLife from './FuncLifeUseE';
+import ShopStock from './ReadShopStock';
 
 function App() {
+  console.log(ShopStock);
   return (
-    // 
+    
     <div>
-     <CounterFunc />
+       <CounterFunc /> 
+       <h1>=============================================================</h1>
       <DropDown />
+      <h1>==============================================================</h1>
        <Parent /> 
-       <FuncLife />
+       <h1>==============================================================</h1>
+        {/* <FuncLife />     */}
+        <h1>===============================================================</h1>
+
+        <ShopStock /> 
+        <h1>================================================================</h1>
+       
       </div>
   );
 }
