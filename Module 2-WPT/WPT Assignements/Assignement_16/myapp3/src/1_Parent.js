591ef80a450b78e9426473ca5d4697adce98e24b
Asit More <136837500+MADARA723@users.noreply.github.com>
2026-03-22 13:42:42 +0530

    Refactor to class component for Parent and Child

--- Module 2-WPT/WPT Assignements/Assignement_16/myapp3/src/1_Parent.js
+++ Module 2-WPT/WPT Assignements/Assignement_16/myapp3/src/1_Parent.js
@@ -5,42 +5,80 @@
 //  add , subtraction , Division , Multiplication 
 // after Option Selection onChange display result 
 // on ParentComponent
-import {useState} from "react";
+import {Component} from "react"
+//import {useState} from "react";
+import Child from "./2_Child";
 
 
-function DropCalc(){
-  function Parent(){
-    let[number1,setNum1]=useState('');
-    let[number2,setNum2]=useState('');
-    
-    function handleNum1Change(n1){
-      return setNum1(n1.target.value);
-    }
-    function handleNum2Change(n2){
-      return setNum2(n2.target.value);
-    }
+class Parent extends Component
+{
+  constructor()
+  {
+    super();
+    this.calculate = this.calculate.bind(this);
+    this.state={
+      num1:"",
+      num2:"",
+      result:""
+    };
   }
-  
-  // function Child(){
-  //   let [Calc,showResult]=useState(['Add','Substraction','Division','Multiplication']);
-  //   let[res,setRes]=useState(0);
-  //        const num1 = parseFloat(number1) || 0;
-  //   const num2 = parseFloat(number2) || 0;
-  //   setRes(num1 + num2);
-  //   }
-   return(
-        <>
-        <input type='number'value={number1} onChange={handleNum1Change}></input>
-        <input type='number' value={number2} onChange={handleNum2change}></input>
-        <select>{Calc.map((item,index)=>
-        <option key={index}>{item}</option>)}
-        </select>
+  handleNum1=(e)=>{
+    this.setState({num1:e.target.value})
+  }
+  handleNum2=(e)=>
+  {
+    this.setState({num2:e.target.value})
+  }
 
-       </>
-   )
+  calculate(operation){
+    let n1=parseFloat(this.state.num1);
+    let n2=parseFloat(this.state.num2);
+
+    let res=0;
+
+    switch(operation){
+      case "add":
+        res=n1+n2;
+        break;
+      case "sub":
+        res=n1-n2;
+        break;
+      case "mul":
+        res=n1*n2;
+        break;
+      case "div":
+        res=n1/n2;
+        break;
+        
+       default:
+        res=0; 
+    }
+
+    this.setState({ result: res });
+  }
+
+  render()
+  {
+    return(
+      <>
+          <h1>Parent</h1>
+
+          Enter Number 1:
+          <input type="number" onChange={this.handleNum1}></input>
+
+          Enter Number 2:
+          <input type="number" onChange={this.handleNum2}></input>
+          {/* Child Component */}
+          <Child onOperation={this.calculate} />
+
+          <h2>Result: {this.state.result}</h2>
 
 
-
-
+      </>
+    )
+  }
 }
-export default DropCalc;
+
+
+
+export default Parent;
