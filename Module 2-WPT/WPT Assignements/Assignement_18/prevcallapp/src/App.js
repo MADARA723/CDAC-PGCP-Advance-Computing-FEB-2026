8885925fc4d9204caccb982e890b5483f5fc6faa
Asit More <136837500+MADARA723@users.noreply.github.com>
2026-03-24 13:01:37 +0530

    Refactor App component and add routing

--- Module 2-WPT/WPT Assignements/Assignement_18/prevcallapp/src/App.js
+++ Module 2-WPT/WPT Assignements/Assignement_18/prevcallapp/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import { ErrorBoundary }
+    from "react-error-boundary";
+import MyFunction from './myFunction';
+import Function2 from './Function2';
+import ErrorHandler, { ErrorCatch } from './ErrorHandler';
 import CallBackPrev from './PrevCall'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './home.js'
+import About from './about.js'
+import ColorPage from './colorPage'
 
 function App() {
   return (
      <div className="App">
-    {/* //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header> */}
+    
 <CallBackPrev />
+
+  <ErrorBoundary FallbackComponent={ErrorCatch}>
+            <MyFunction></MyFunction>
+   </ErrorBoundary>
+      <Function2></Function2>
+       <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ColorPage color="cyan" />} />
+        <Route path="/red" element={<ColorPage color="red" />} />
+        <Route path="/yellow" element={<ColorPage color="yellow" />} />
+        <Route path="/green" element={<ColorPage color="green" />} />
+        <Route path="/blue" element={<ColorPage color="blue" />} />
+        <Route path="/orange" element={<ColorPage color="orange" />} />
+      </Routes>
+    </BrowserRouter>
+  
     </div>
   );
 }
