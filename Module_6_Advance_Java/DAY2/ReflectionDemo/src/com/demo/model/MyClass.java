7b639d3bb91188fdcf3b6f066251b89a5bc3d839
Asit More <136837500+MADARA723@users.noreply.github.com>
2026-05-20 16:32:34 +0530

    Add files via upload

---
+++ Module_6_Advance_Java/DAY2/ReflectionDemo/src/com/demo/model/MyClass.java
@@ -0,0 +1,32 @@
+package com.demo.model;
+
+public class MyClass {
+	private int id;
+	private String  nm;
+	public MyClass() {
+		super();
+	}
+	public MyClass(int id, String nm) {
+		super();
+		this.id = id;
+		this.nm = nm;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNm() {
+		return nm;
+	}
+	public void setNm(String nm) {
+		this.nm = nm;
+	}
+	@Override
+	public String toString() {
+		return "MyClass [id=" + id + ", nm=" + nm + "]";
+	}
+	
+
+}
