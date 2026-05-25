f62fcb76b4a75150a6b6183c1e219f8a37a36473
Asit More <136837500+MADARA723@users.noreply.github.com>
2026-05-25 16:42:48 +0530

    Add files via upload

---
+++ Module_6_Advance_Java/DAY5/HiddenFieldsDemo/src/main/java/com/demo/servlets/DsiplayServlet.java
@@ -0,0 +1,28 @@
+package com.demo.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class DsiplayServlet
+ */
+@WebServlet("/display")
+public class DsiplayServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String name=request.getParameter("name");
+		String email=request.getParameter("email");
+		out.println("Name : "+name);
+		out.println("Email: "+email);
+	}
+
+	
+
+}
