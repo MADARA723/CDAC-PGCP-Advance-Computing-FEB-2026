894c28183ea2a2573a09f112adc6ee41363c4c59
Asit More <asitmore23@gmail.com>
2026-06-04 16:46:12 +0530

    Added SpringMVCDEMO

---
+++ Module_6_Advance_Java/DAY12/SpringBoot/SpringMVCDemo2/src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,33 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String sayHello()
+	{
+		return "Login";
+	}
+	
+	@RequestMapping("/test")
+	public String gettest(Model model)
+	{
+		model.addAttribute("m1","This msg is from m1");
+		model.addAttribute("m2","Thsi msg from m2");
+		return "testmodel";
+	}
+	
+	//@RequestMapping(value="/hello",method=RequestMethod.GET)
+	@GetMapping("/hello")//Newer method
+	public ModelAndView getMessage()
+	{
+		String msg="Hello from getMessage";
+		return new ModelAndView("greet","message",msg);
+	}
+}
---
+++ Module_6_Advance_Java/DAY12/SpringBoot/SpringMVCDemo2/src/main/java/com/demo/service/LoginServiceImpl.java
@@ -0,0 +1,21 @@
+package com.demo.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.beans.MyUser;
+import com.demo.dao.LoginDao;
+
+@Service
+public class LoginServiceImpl implements LoginService{
+	@Autowired
+	private LoginDao ldao;
+
+	@Override
+	public MyUser validateUser(String u1, String pass) {
+		System.out.println("ValidateUSer");
+		return ldao.authenticteUser(u1,pass);
+	}
+
+}
